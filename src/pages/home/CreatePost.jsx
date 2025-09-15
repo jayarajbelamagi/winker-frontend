bad2534e6f88dea430defb09060473a01ee88bdd
Jayaraj U B <ubelamagi@gmail.com>
2025-09-16 00:53:47 +0530

    Update CreatePost.jsx

--- src/pages/home/CreatePost.jsx
+++ src/pages/home/CreatePost.jsx
@@ -4,7 +4,7 @@ import { useRef, useState } from "react";
 import { IoCloseSharp } from "react-icons/io5";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-hot-toast";
-import { apiFetch } from "../../utils/apiFetch";
+import { apiFetch } from "../../services/apiClient";
 
 const CreatePost = () => {
   const [text, setText] = useState("");
